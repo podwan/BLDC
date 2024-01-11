fd1a504083b6cef2c56555132918a822cbee3637
inffydwan <7534571+inffydwan@user.noreply.gitee.com>
2024-01-11 23:00:38 +0800

    霍尔闭环中

---
+++ firmware/closeLoop/User/common.h
@@ -0,0 +1,16 @@
+#ifndef __COMMON_H
+#define __COMMON_H
+#include "stm32g4xx_hal.h"
+#include "math_utils.h"
+
+
+
+typedef enum
+{
+    false,
+    true
+} bool;
+
+
+
+#endif
---
+++ firmware/closeLoop/User/sensor.c
@@ -0,0 +1,10 @@
+#include "sensor.h"
+
+bool adc_isr_flag = 0;
+bool opa_cali_flag = 0;
+
+float positionEstimate(float uD, float uQ, float iD, float iQ)
+{
+}
+
+
