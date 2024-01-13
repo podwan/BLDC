f77a325158fe774d8ce18ad7203e5dd82a0fba30
inffydwan <7534571+inffydwan@user.noreply.gitee.com>
2024-01-13 22:58:36 +0800

    准备交换代码测试霍尔测速

--- firmware/closeLoop/Core/Src/gpio.c
+++ firmware/closeLoop/Core/Src/gpio.c
@@ -74,7 +74,7 @@ void MX_GPIO_Init(void)
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
   /* EXTI interrupt init*/
-  HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
+  HAL_NVIC_SetPriority(EXTI15_10_IRQn, 3, 0);
   HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
 
 }
